cc5a1ae6b2fd5590e29e724da7ca243541efe111
william <laurencius.william@gmail.com>
2023-07-03 16:13:21 +0700

    feat:crud user

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,10 @@ router
   .post("/login", UserController.login)
   .post("/eventregister/:id", authentiaction, UserController.regisEvent)
   .get("/mylist", authentiaction, UserController.getMyList)
-  .get("/mytodo/:id", authentiaction, UserController.getMyTodo);
+  .get("/mytodo/:id", authentiaction, UserController.getMyTodo)
+  .get("/users", authentiaction, UserController.getProfile)
+  .put("/users", authentiaction, UserController.updateAccount)
+  .put("/profile", authentiaction, UserController.updateProfile)
+  .post("/verify", authentiaction, UserController.verifyAccount);
 
 module.exports = router;
