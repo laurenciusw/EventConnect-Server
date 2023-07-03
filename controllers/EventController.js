76032e34d474b4c3915498d3d08e35afba304a7e
petersatria <180710018@students.uajy.ac.id>
2023-07-03 14:56:04 +0700

    feat: include association event

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,10 +1,26 @@
 
-const { Event, Benefit, sequelize, JobDesk } = require('../models')
+const { Event, Benefit, sequelize, JobDesk, Organizer } = require('../models')
 
 class EventController {
   static async getEvent(req, res, next) {
     try {
-      const fetchEvent = await Event.findAll()
+      const fetchEvent = await Event.findAll({
+        include: [{ model: Organizer }, { model: Benefit }],
+      })
+
+      res.status(200).json(fetchEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+  static async getEventById(req, res, next) {
+    try {
+      const { id } = req.params
+      const fetchEvent = await Event.findByPk(id, {
+        include: [{ model: Organizer }, { model: Benefit }, { model: JobDesk }],
+      })
+
+      if (!fetchEvent) throw { name: 'NotFound' }
 
       res.status(200).json(fetchEvent)
     } catch (error) {
@@ -14,12 +30,14 @@ class EventController {
 
   static async getEventByOrganizerId(req, res, next) {
     try {
-      const {OrganizerId} = req.user
+      const { OrganizerId } = req.user
       // const OrganizerId = req.headers.organizerid
-      
-      const fetchEvent = await Event.findAll({where: {
-        OrganizerId
-      }})
+
+      const fetchEvent = await Event.findAll({
+        where: {
+          OrganizerId
+        }
+      })
 
       res.status(200).json(fetchEvent)
 
@@ -35,11 +53,11 @@ class EventController {
       // bentuk benefit = [{"name": "blablabla"}, {"name": "blablabla"}]
       // bentuk jobdesk = [{"name": "blablabla"}, {"name": "blablabla"}]
 
-      const {OrganizerId} = req.user
+      const { OrganizerId } = req.user
       // const OrganizerId = req.headers.organizerid
-      
+
       const result = await sequelize.transaction(async (t) => {
-        
+
         const newEvent = await Event.create({ name, location, startDate, imageUrl, description, endDate, registrationDate, category, status, OrganizerId }, { transaction: t });
 
         const benefitParsed = await JSON.parse(benefit)
@@ -79,7 +97,7 @@ class EventController {
         throw { name: 'NotFound' }
       }
 
-      const updatedEvent = await Event.update({ name, location, startDate, imageUrl, description, endDate, registrationDate, category, status}, {
+      const updatedEvent = await Event.update({ name, location, startDate, imageUrl, description, endDate, registrationDate, category, status }, {
         where: {
           id
         }
@@ -95,7 +113,7 @@ class EventController {
   static async deleteEvent(req, res, next) {
     try {
       const { id } = req.params
-      
+
       const findEvent = await Event.findByPk(id)
       if (!findEvent) {
         throw { name: 'NotFound' }
